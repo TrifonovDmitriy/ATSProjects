cf197299138a86195a21758eba1f0f1ffff917fe
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-04-22 20:57:31 +0700

    Задача 1-7

---
+++ src/main/java/ru/task1_7/Main.java
@@ -0,0 +1,10 @@
+package ru.task1_7;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 23; i++) {  // выведем результат 2х десятков для проверки
+            System.out.println(Task7.getProgrammer(i));
+        }
+
+    }
+}

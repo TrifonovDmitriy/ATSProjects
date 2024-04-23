7fd0ad24a6cbdce0f95351f191b6092297824cb1
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-04-23 14:19:54 +0700

    Задача 2-8

---
+++ src/main/java/ru/task2_8/Main.java
@@ -0,0 +1,9 @@
+package ru.task2_8;
+
+public class Main {
+    public static void main(String[] args) {
+        Task8.triangle1(4);
+        System.out.println();
+        Task8.triangle2(4);
+    }
+}
